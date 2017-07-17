c75d7e0fd3028476e58219e76ec725126c2b512e
Shurpanchik <Shur1993@mail.ru>
2017-07-17 15:47:29 +0300

    Add files via upload

---
+++ WebApi/Test2/TextServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApi.Api;
+
+namespace Test2
+{
+    class TextServiceTests
+    {
+        [Test]
+        public void BadWordTest()
+        {
+            Assert.AreEqual(true, TextService.IsBadText("Bad word"));
+        }
+
+        [Test]
+        public void GoodWordTest()
+        {
+            Assert.AreEqual(false, TextService.IsBadText("Good word"));
+        }
+    }
+}
