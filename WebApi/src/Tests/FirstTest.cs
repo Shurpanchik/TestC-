33a4e742e751ce4b29116a4e9acd9d35e077a05f
Shurpanchik <Shur1993@mail.ru>
2017-07-17 13:41:36 +0300

    Add files via upload
    тесты

---
+++ WebApi/src/Tests/FirstTest.cs
@@ -0,0 +1,54 @@
+﻿
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using WebApi.Models;
+using WebApi.Api;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class FirstTest
+    {
+        
+        [Test]
+        public void PassingTest() { Assert.AreEqual(2, 1 << 1); }
+
+        [Test]
+        public void FailingTest() { Assert.AreEqual(2, 1 >> 1); }
+
+        [Test]
+        public void DbContext()
+        {
+            var data = new List<Message>
+            {
+                new Message
+                {
+                    Id = Guid.NewGuid(),
+                    CreateDate = DateTimeOffset.UtcNow,
+                    Text = Guid.NewGuid().ToString()
+                },
+                new Message
+                {
+                    Id = Guid.NewGuid(),
+                    CreateDate = DateTimeOffset.UtcNow,
+                   Text = Guid.NewGuid().ToString()
+                }
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Message>>();
+            mockSet.As<IQueryable<Message>>().Setup(__messages => __messages.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Message>>().Setup(__messages => __messages.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Message>>().Setup(__messages => __messages.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Message>>().Setup(__messages => __messages.GetEnumerator()).Returns(() => data.GetEnumerator());
+
+            var mockContext = new Mock<ApiDbContext>();
+            mockContext.Setup(c => c.Messages).Returns(mockSet.Object);
+
+            Assert.AreEqual(2, mockContext.Object.Messages.Count());
+        }
+    }
+}
---
+++ WebApi/src/Tests/MessagServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApi.Api;
+using WebApi.Models;
+
+namespace Test2
+{
+    class MessageServiceTests
+    {
+        Mock<ApiDbContext> mockContext;
+
+        public MessageServiceTests()
+        {
+            mockContext = new Mock<ApiDbContext>();
+            mockContext.Setup(m => m.Add(It.IsAny<Message>())).Verifiable();
+
+            mockContext.Setup(m => m.SaveChanges()).Verifiable();
+
+        }
+
+        [Test]
+        public void AddMessageWithBadWords()
+        {
+            AddMessage("Bad word");
+
+            mockContext.Verify(m => m.Add(It.IsAny<Message>()), Times.Never);
+            mockContext.Verify(m => m.SaveChanges(), Times.Never);
+        }
+        [Test]
+        public void AddMessageWithPositiveWords()
+        {
+            AddMessage("It is good day");
+
+            //mockContext.Verify(m => m.Add(It.IsAny<Message>()), Times.Once);
+            mockContext.Verify(m => m.SaveChanges(), Times.Once);
+        }
+
+
+        void AddMessage(string text)
+        {
+            MessagesService messagesService = new MessagesService(mockContext.Object);
+            messagesService.Add(new Message
+            {
+                Id = Guid.NewGuid(),
+                CreateDate = DateTimeOffset.UtcNow,
+                Text = text
+            });
+        }
+    }
+}
